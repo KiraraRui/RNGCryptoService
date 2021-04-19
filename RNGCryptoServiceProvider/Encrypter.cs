2ce2d6238e287dcb03a9e81f6d52213feff08992
Kira Michelle Achen <kira0698@zbc.dk>
2021-04-19 19:00:48 +0200

    first commit

---
+++ RNGCryptoServiceProvider/Encrypter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace RNGCryptoServiceProvider1
+{
+    internal class Encrypter
+    {
+        private static readonly char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public static string Encrypt(string message)
+        {
+            char[] secretMessage = message.ToCharArray();
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char currentChar = secretMessage[i];
+                int letterPosition = Array.IndexOf(alphabet, currentChar);
+
+                // Modulus Operator and remainder of after an integer division (not to self mostly)
+                int shiftedletterPosition = (letterPosition += 1) % alphabet.Length;
+
+                //this one assigns the value after it has been shifted
+                encryptedMessage[i] = alphabet[shiftedletterPosition];
+            }
+            return new string(encryptedMessage);
+        }
+
+
+        public static string Dencrypt(string demessage)
+        {
+            char[] secretMessage = demessage.ToCharArray();
+            char[] decryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char currentChar = secretMessage[i];
+                int letterPosition = Array.IndexOf(alphabet, currentChar);
+
+                // Modulus Operator and remainder of after an integer division (not to self mostly)
+                int shiftedletterPosition = (letterPosition += -1) % alphabet.Length;
+
+                //this one assigns the value after it has been shifted
+                decryptedMessage[i] = alphabet[shiftedletterPosition];
+            }
+            return new string(decryptedMessage);
+        }
+
+
+    }
+}
+
+
---
+++ RNGCryptoServiceProvider/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNGCryptoServiceProvider1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                stopwatch.Start();
+                byte[] data = new byte[4];
+
+                for (int i = 0; i < 10; i++)
+                {
+                    rng.GetBytes(data);
+
+                    int value = BitConverter.ToInt32(data, 0);
+                    Console.WriteLine("the value of RNGCryptoServiceProvider byte is " + value);
+                    Console.WriteLine("the value of RNGCryptoServiceProvider base62 is" + Convert.ToBase64String(data));
+
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed.TotalMilliseconds);
+            Console.WriteLine("----------------------------------------------------");
+
+            var rand = new Random();
+             {
+                byte[] data = new byte[4];
+                stopwatch.Start();
+                var bytes = new byte[4];
+
+                for (int i = 0; i < 10; i++)
+                {
+                    rand.NextBytes(bytes);
+
+
+                    int value = BitConverter.ToInt32(bytes, 0);
+                    Console.WriteLine("the value of Rand byte is " + value);
+                    Console.WriteLine("the value of Rand base62 is " + Convert.ToBase64String(bytes));
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed.TotalMilliseconds);
+            Console.ReadKey();
+
+
+            Console.WriteLine("------------Press enter for Encryption---------------");
+
+
+            Console.WriteLine("type a message to encrypt it.");
+            string message = Console.ReadLine();
+
+            Console.Write(Encrypter.Encrypt(message));
+            
+
+
+
+
+
+            Console.WriteLine("\n------------Press enter for decryption---------------");
+
+
+            Console.WriteLine("type a message to decrypt it.");
+            string demessage = Console.ReadLine();
+
+            Console.Write(Encrypter.Dencrypt(demessage));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
